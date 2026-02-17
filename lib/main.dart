d60c0313a6a6f0ce2aba062708fad802ffdbf719
HamzaKherazi <hamza.kherazi.7@gmail.com>
2026-02-17 11:02:00 +0000

    create splash view

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:bookly/core/constants.dart';
+import 'package:bookly/features/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: kPrimaryColor, // Background color
+    ),
+  );
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: kPrimaryColor,
+        brightness: Brightness.dark,
+      ),
+
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
