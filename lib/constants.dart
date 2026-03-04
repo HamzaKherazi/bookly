2a0e7f741807602b03b61a557da981a41bc25b4c
HamzaKherazi <hamza.kherazi.7@gmail.com>
2026-03-04 11:13:29 +0000

    add app text styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/widgets.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kAnimationDuration = Duration(milliseconds: 250);
+const kgTSectraFine = 'GT-Sectra-Fine';
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -23,7 +24,86 @@ class HomeViewBody extends StatelessWidget {
             child: BooksListView(),
           ),
           SizedBox(height: 60),
-          Text('Best Seller', style: Styles.titleMedium),
+          Text('Best Seller', style: Styles.textStyle18),
+          SizedBox(height: 30),
+          BestSellerItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerItem extends StatelessWidget {
+  const BestSellerItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: Image.network(
+                    'https://m.media-amazon.com/images/I/71bmxf4xUsL._AC_UF894,1000_QL80_.jpg',
+                  ).image,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'The Sealed Nectar',
+                style: TextStyle(
+                  fontFamily: kgTSectraFine,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Mubarakfuri',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: const Color.fromARGB(255, 207, 207, 207),
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '19.99',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.star, color: Colors.white),
+                  ),
+                  Text(
+                    '4.8',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Text('(2390)'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
