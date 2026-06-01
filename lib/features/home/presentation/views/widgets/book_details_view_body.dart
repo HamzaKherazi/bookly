37cf8bcb90d253fb205de02d996cd463c7c8c80a
HamzaKherazi <hamza.kherazi.7@gmail.com>
2026-06-01 18:32:47 +0100

    add title and subtitle to details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
@@ -9,13 +10,25 @@ class BookDetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+      padding: EdgeInsets.symmetric(horizontal: width * 0.24),
       child: Column(
         children: [
           const CustomBookImage(),
-          const Text('Raheeq', style: TextStyle(fontSize: 24)),
-          const Text('Raheeq', style: TextStyle(fontSize: 24)),
-          const Text('Raheeq', style: TextStyle(fontSize: 24)),
+          const SizedBox(height: 43),
+          Text(
+            'Al-Raheeq',
+            maxLines: 2,
+            style: Styles.textStyle30,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 6),
+          Text(
+            'Al-Mubarakfuri',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
           const BookRating(),
         ],
       ),
